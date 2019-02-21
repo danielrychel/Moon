0c611ddd22837d17d9031830c7b7a2dffd01c1f8
Sketching101 <aras.yazgan@gmail.com>
2019-02-21 15:01:16 -0600

    Edited v1

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float MaxHP;
+    public float RemainingHP;
+
+    public bool alive = true;
+
+    void Start()
+    {
+        RemainingHP = MaxHP;
+    }
+
+    // Update is called once per frame
+    public bool takeDamage(float dmg)
+    {
+        RemainingHP -= dmg;
+        if (RemainingHP <= 0)
+        {
+            alive = false;
+            Debug.Log("Killed!");
+            return true;
+        }
+        Debug.Log("Damage!");
+        return false;
+    }
+
+    public void takeHeal(float heal)
+    {
+        if ((RemainingHP += heal) > MaxHP)
+            RemainingHP = MaxHP;
+        else
+            RemainingHP += heal;
+    }
+}
