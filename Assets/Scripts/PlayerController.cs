d5fcdf2037b1de6c46bd6841ab4a86d16dc767d2
Daniel Rychel <danielrrychel@gmail.com>
2019-03-10 18:42:43 -0500

    Copied files over from master

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,14 @@ public class PlayerController : MonoBehaviour
     public float maxSpeed;
     public Transform gunPivot;
     public Transform gun;
+    public Transform corpse;
 
     public float camDistance;
     public Transform cam;
 
     public float swordDmg, colDmg;
+
+    public Health hp;
 
 
     public Transform bullet;
@@ -27,51 +30,62 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // rotate gun
-        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 mouseDelta = mouseWorld - gunPivot.position;
-        float angle = Mathf.Atan2(mouseDelta.y, mouseDelta.x);
-        gunPivot.rotation = Quaternion.Euler(0, 0, angle * 180 / Mathf.PI);
-
-        // translate camera
-        Vector3 camTarget = new Vector3(camDistance*Mathf.Cos(angle), camDistance*Mathf.Sin(angle), -10);
-        cam.localPosition = Vector3.Lerp(cam.localPosition, camTarget, 0.1f);
-
-        if (Input.GetButtonDown("Fire1"))
+        if (hp.alive)
         {
-            var shoot = Instantiate(bullet, gun.position, gun.rotation);
-            shoot.tag = "PlayerBullet";
+            Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mouseDelta = mouseWorld - gunPivot.position;
+            float angle = Mathf.Atan2(mouseDelta.y, mouseDelta.x);
+            gunPivot.rotation = Quaternion.Euler(0, 0, angle * 180 / Mathf.PI);
+
+            // translate camera
+            Vector3 camTarget = new Vector3(camDistance * Mathf.Cos(angle), camDistance * Mathf.Sin(angle), -10);
+            cam.localPosition = Vector3.Lerp(cam.localPosition, camTarget, 0.1f);
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Instantiate(bullet, gun.position, gun.rotation);
+            }
         }
     }
 
     void FixedUpdate()
     {
-        if (!dashLogic.dashing)
+        if (!dashLogic.dashing && hp.alive)
         {
             float hSpeed = Input.GetAxis("Horizontal");
             float vSpeed = Input.GetAxis("Vertical");
             Vector2 v = new Vector2(hSpeed, vSpeed);
             rb2d.velocity = v * maxSpeed;
         }
+        if (!hp.alive)
+        {
+            GetComponent<SpriteRenderer>().sprite = corpse.gameObject.GetComponent<SpriteRenderer>().sprite;
+            //           Instantiate(corpse, new Vector3(rb2d.gameObject.transform.position.x + 1f, rb2d.gameObject.transform.position.y - 0.7f, rb2d.gameObject.transform.position.z), Quaternion.identity);
+            //   Die.exe;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Collision detected");
-        if (collision.tag == "Killable" && dashLogic.frame == DashAbility.Frames.Damage)
+        if (hp.alive)
         {
-            if (collision.gameObject.GetComponent<Health>().takeDamage(swordDmg))
-                dashLogic.setKilled();
-            Debug.Log("Contact");
-        }
-        else if (collision.tag == "Killable")
-        {
-            GetComponent<Health>().takeDamage(colDmg);
-            // Take damage if contact in vulnerable 
-        }
-        else if (collision.tag == "teleporter")
-        {
-            transform.position = new Vector3(0f, -1.5f, 0.0f);
-        
+            Debug.Log("Collision detected");
+            if (collision.tag == "Killable" && dashLogic.frame == DashAbility.Frames.Damage)
+            {
+                if (collision.gameObject.GetComponent<Health>().takeDamage(swordDmg))
+                    dashLogic.setKilled();
+                Debug.Log("Contact");
+            }
+            else if (collision.tag == "Killable")
+            {
+                GetComponent<Health>().takeDamage(colDmg);
+                // Take damage if contact in vulnerable 
+            }
+            else if (collision.tag == "teleporter")
+            {
+                transform.position = new Vector3(0f, -1.5f, 0.0f);
+
+            }
         }
     }
 }
