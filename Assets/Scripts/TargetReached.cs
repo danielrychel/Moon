c2823026360a09d8a2ab53fb45597c70e638afeb
Sketching101 <aras.yazgan@gmail.com>
2019-04-02 14:13:15 -0500

    Update from master

---
+++ Assets/Scripts/TargetReached.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+
+public class TargetReached : AIPath
+{
+    private Transform nextTarget;
+    private bool arrived = false;
+    public AIDestinationSetter moveTo;
+
+    //void Start()
+    //{
+    //    this.IAstarAI.radius = 1;
+    //}
+
+    public bool SetNextTarget(Transform newTarget)
+        {
+            nextTarget = newTarget;
+            if(arrived)
+            {
+                arrived = false;
+                return true;
+            }
+            arrived = false;
+            return false;
+        }
+
+    public override void OnTargetReached()
+    {
+        gameObject.GetComponent<AIDestinationSetter>().target = nextTarget;
+        arrived = true;
+    }
+
+
+    private void SetMoveTo(Transform target)
+    {
+        if (moveTo.target != target) moveTo.target = target;
+    }
+}
+
+
