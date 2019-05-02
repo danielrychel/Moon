4100e218d6775694708799012b823922a2d6ef2e
bbernardoni <bennett.bernardoni@gmail.com>
2019-05-01 22:56:12 -0500

    Switch text for door tut

--- Assets/Scripts/DoorTutorial.cs
+++ Assets/Scripts/DoorTutorial.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DoorTutorial : MonoBehaviour
 {
     public door_control door;
     public GameObject tutorial;
     public float range;
+    public Text tut;
 
     // Start is called before the first frame update
     void Start()
@@ -23,5 +25,10 @@ public class DoorTutorial : MonoBehaviour
         else {
             tutorial.SetActive(false);
         }
+
+        if(GameManager.instance.useKeyboard)
+            tut.text = "Press E";
+        else
+            tut.text = "Press A";
     }
 }
