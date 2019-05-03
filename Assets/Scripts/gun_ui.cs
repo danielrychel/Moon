dabef700ecad2da85e4ead52cc322733d287a6cf
WillXiaoWei <weixiaothink@outlook.com>
2019-05-03 14:22:01 -0500

    Revert "Revert "railgun implemented""
    This reverts commit 265ba77696847a0f9cab42e724928e3258081e01.

--- Assets/Scripts/gun_ui.cs
+++ Assets/Scripts/gun_ui.cs
@@ -10,6 +10,7 @@ public class gun_ui : MonoBehaviour
     public Image gun_image;
     public Sprite pistol;
     public Sprite shotgun;
+    public Sprite railgun;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +26,9 @@ public class gun_ui : MonoBehaviour
                 break;
             case "Shotgun1":
                 gun_image.sprite = shotgun;
+                break;
+            case "Railgun1":
+                gun_image.sprite = railgun;
                 break;
             default:
                 gun_image.sprite = pistol;
